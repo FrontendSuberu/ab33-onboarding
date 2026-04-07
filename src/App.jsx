4cb56e4fe516a8b7f6d702ff8c644f8d9586c0ac
FrontendSuberu <joeysuberu@gmail.com>
2026-04-07 01:45:07 +0100

    form structure updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import logo from "/logo.svg";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      </>
-    
-  )
+    <main data-form>
+      <header className="verif-form-header">
+        <img src={logo} alt="ab33 logo" />
+      </header>
+
+      <div className="form-wrapper">
+        <h1>Sign Up</h1>
+        <form method="post">
+          <div data-form-group>
+            <label htmlFor="fullName">
+              <span>FULL NAME</span>
+              <input type="text" />
+            </label>
+
+            <label htmlFor="email">
+              <span>EMAIL</span>
+              <input type="email" />
+            </label>
+
+            <label htmlFor="password">
+              <span>PASSWORD</span>
+              <input type="password" />
+            </label>
+          </div>
+
+          <span className="c-new-acct">Don't have an account? create one here</span>
+
+          <div className="form-btn-group">
+            <button>Create my account</button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
