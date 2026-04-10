5acadcb76723220087f07368ceff96a432128f51
FrontendSuberu <joeysuberu@gmail.com>
2026-04-10 23:31:04 +0100

    added autofocus to input for better mobile experience

--- src/pages/verify/components/InputOtp.jsx
+++ src/pages/verify/components/InputOtp.jsx
@@ -8,13 +8,13 @@ import {
 export default function InputOtp({otp, setOtp}) {
   return (
     <div className="h-15 flex justify-center items-center">
-      <InputOTP value={otp} onChange={setOtp} id="digits-only" maxLength={5} pattern={REGEXP_ONLY_DIGITS}>
+      <InputOTP value={otp} autoFocus onChange={setOtp} id="digits-only" maxLength={5} pattern={REGEXP_ONLY_DIGITS}>
         <InputOTPGroup>
-          <InputOTPSlot index={0} className="h-15 w-15 " />
-          <InputOTPSlot index={1} className="h-15 w-15" />
-          <InputOTPSlot index={2} className="h-15 w-15" />
-          <InputOTPSlot index={3} className="h-15 w-15" />
-          <InputOTPSlot index={4} className="h-15 w-15 " />
+          <InputOTPSlot index={0} className="w-12 h-12 md:h-15  md:w-15 " />
+          <InputOTPSlot index={1} className="w-12 h-12 md:h-15  md:w-15" />
+          <InputOTPSlot index={2} className="w-12 h-12 md:h-15  md:w-15" />
+          <InputOTPSlot index={3} className="w-12 h-12 md:h-15  md:w-15" />
+          <InputOTPSlot index={4} className="w-12 h-12 md:h-15  md:w-15 " />
         </InputOTPGroup>
       </InputOTP>
     </div>
