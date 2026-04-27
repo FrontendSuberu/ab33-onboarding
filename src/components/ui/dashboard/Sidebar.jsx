2fab473ed76be341be6850288594d8f85ffb416e
FrontendSuberu <joeysuberu@gmail.com>
2026-04-27 23:43:08 +0100

    added more markup content to the component

--- src/components/ui/dashboard/Sidebar.jsx
+++ src/components/ui/dashboard/Sidebar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "/logoMain.svg"
+import Logo from "/logoMain.svg";
 
 export default function Sidebar() {
   return (
-    <div className="bg-white w-75">
-      {/* header */}
+    <div className="bg-white w-75 relative">
+      {/* button toggle to close and open component */}
+      <button className="absolute h-screen right-[-40px]">
+        <span className="bg-gray-400">toggle</span>
+      </button>
 
+      {/* header */}
       <div>
         <img src={Logo} alt="ab33" />
       </div>
